156a6af55156b15690ad01e259a1ca314963f58d
qinpr <peirongq_1@tujia.com>
2019-02-19 14:04:36 +0800

    cm

---
+++ follow-all/message-queue/store/src/main/java/com/qinpr/follow/messageQueue/store/Test.java
@@ -0,0 +1,10 @@
+package com.qinpr.follow.messageQueue.store;
+
+/**
+ * Created by qinpr on 2019/2/19.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("000000000000000000000000000");
+    }
+}
